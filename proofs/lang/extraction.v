997aa99ba38d3dac9c5c05e404ee109c94e1987e
Lionel Blatter <lionel.blatter@mpi-sp.org>
2025-03-18 15:25:57 +0100

    Add support for abstract types
    Co-authored-by: Benjamin Gregoire <Benjamin.Gregoire@inria.fr>

--- proofs/lang/extraction.v
+++ proofs/lang/extraction.v
@@ -51,6 +51,10 @@ Extract Constant ident.Cident.id_kind => "CoreIdent.Cident.id_kind".
 
 Extract Constant ident.Cident.spill_to_mmx => "CoreIdent.Cident.spill_to_mmx".
 
+(* Module Cabstract *)
+
+Extract Constant abstract_type.Cabstract.iabstract => "AbstractType.Cabstract.iabstract".
+
 Cd  "lang/ocaml".
 
 Extraction Blacklist String List Nat Uint63 Utils Var Array.
